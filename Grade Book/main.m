8c80052a4a921f141079d9d4a9bba8f1fcf8a2d8
Tara Weinreb <ataraweinreb@gmail.com>
2019-10-02 14:12:25 -0400

    Initial Commit

---
+++ Grade Book/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Grade Book
+//
+//  Created by Atara Weinreb on 10/2/19.
+//  Copyright © 2019 Atara Weinreb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
