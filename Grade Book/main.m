9864ad0fbb94fdb71ae9d701ddd93e1d3c8bd9a0
Tara Weinreb <ataraweinreb@gmail.com>
2019-10-02 20:11:07 -0400

    Finished project.

--- Grade Book/main.m
+++ Grade Book/main.m
@@ -12,8 +12,49 @@
 
 NSMutableDictionary *myDictionary;
 
+void removeAllStudents() {
+    if ([myDictionary count] == 0)
+        return;
+    [myDictionary removeAllObjects];
+}
+
+void removeStudent(NSString *name) {
+    if (![myDictionary objectForKey:name]) {
+        return;
+    }
+    [myDictionary removeObjectForKey:name];
+}
+
+BOOL studentAverage(NSString *name) {
+    if (![myDictionary objectForKey:name])
+        return false;
+    
+    Student_Info *student = [myDictionary objectForKey:name];
+    
+    float midterm = [student Midterm];
+    float final = [student Final];
+    int homework1 = [student Homework1];
+    int homework2 = [student Homework2];
+    int homework3 = [student Homework3];
+    int homework = (homework1 + homework2 + homework3) * 10 /3; //homework is graded out of 10
+    
+    if (midterm < 0 || final < 0 || homework1 < 0 || homework2 < 0 || homework3 < 0)
+        return false;
+    float avg = (midterm * .30) + (final * .40) + (homework * .30);
+    NSLog(@"Student average: %.1f", avg);
+    return true;
+}
+
 void printObject(Student_Info *student) {
-    NSLog(@"Name: %@\n\tAddress: %@\n\tMidterm: %.1f\n\tFinal: %.1f\n\tHomework #1: %.d\n\tHomework #2: %d\n\tHomework #3: %d\n\t", (NSString *)student.Name, (NSString *)student.Address, student.Midterm, student.Final, student.Homework1, student.Homework2, student.Homework3);
+    NSLog(@"\n****************************************\n\n\n\tName: %@\n\tAddress: %@\n\tMidterm: %.1f\n\tFinal: %.1f\n\tHomework #1: %.d\n\tHomework #2: %d\n\tHomework #3: %d\n\n\n****************************************", (NSString *)student.Name, (NSString *)student.Address, student.Midterm, student.Final, student.Homework1, student.Homework2, student.Homework3);
+}
+
+void enumerateAllStudents() {
+    for(NSString *key in myDictionary) {
+        Student_Info *temp = [myDictionary objectForKey:key];
+        NSLog(key);
+        printObject(temp);
+    }
 }
 
 BOOL addStudent(NSString *name, NSString *address) {
@@ -66,44 +107,73 @@ BOOL addHomework(NSString *name, int assignment, int score) {
     return true;
 }
 
-BOOL studentAverage(NSString *name) {
-    if (![myDictionary objectForKey:name])
-        return false;
-    
-    Student_Info *student = [myDictionary objectForKey:name];
-    
-    float midterm = [student Midterm];
-    float final = [student Final];
-    int homework1 = [student Homework1];
-    int homework2 = [student Homework2];
-    int homework3 = [student Homework3];
-    int homework = (homework1 + homework2 + homework3) * 10 /3; //homework is graded out of 10
-    
-    if (midterm < 0 || final < 0 || homework1 < 0 || homework2 < 0 || homework3 < 0)
-        return false;
-    float avg = (midterm * .30) + (final * .40) + (homework * .30);
-    NSLog(@"Student average: %.1f", avg);
-    return true;
-}
-
 
 int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
-        NSLog(@"Hello, World!");
-        addStudent(@"harry", @"310 eastwood rd");
-        addStudent(@"harry", @"310 eastwood rd");
-        addStudent(@"hkhsadh", @"310 eastwood rd");
-        addTest(@"harry", 75, @"midterm");
-        addTest(@"harry", 87, @"final");
-        addHomework(@"harry", 1, 10);
-        addHomework(@"harry", 2, 10);
-        addHomework(@"harry", 3, 10);
-        studentAverage(@"harry");
-        for(NSString *key in myDictionary) {
-            Student_Info *temp = [myDictionary objectForKey:key];
-            printObject(temp);
-        }
+        
+        //Student #1
+        addStudent(@"Bob", @"100 Central Ave");
+        addTest(@"Bob", 85, @"midterm");
+        addTest(@"Bob", 95, @"final");
+        addHomework(@"Bob", 1, 7);
+        addHomework(@"Bob", 2, 6);
+        addHomework(@"Bob", 3, 10);
+        
+        //Student #2
+        addStudent(@"Molly", @"80 Charles St");
+        addTest(@"Molly", 70, @"midterm");
+        addTest(@"Molly", 65, @"final");
+        addHomework(@"Molly", 1, 7);
+        addHomework(@"Molly", 2, 4);
+        addHomework(@"Molly", 3, 9);
+        
+        //Student #3
+        addStudent(@"Carol", @"12 Virginia St");
+        addTest(@"Carol", 100, @"midterm");
+        addTest(@"Carol", 98.5, @"final");
+        addHomework(@"Carol", 1, 9);
+        addHomework(@"Carol", 2, 10);
+        addHomework(@"Carol", 3, 10);
+        
+        //Student #4
+        addStudent(@"David", @"44 Crestwood Rd");
+        addTest(@"David", 77, @"midterm");
+        addTest(@"David", 82.5, @"final");
+        addHomework(@"David", 1, 10);
+        addHomework(@"David", 2, 3);
+        addHomework(@"David", 3, 6);
+        
+        //Student #5
+        addStudent(@"Sarah", @"111 Woodmere Place");
+        addTest(@"Sarah", 45, @"midterm");
+        addTest(@"Sarah", 55, @"final");
+        addHomework(@"Sarah", 1, 6);
+        addHomework(@"Sarah", 2, 2);
+        addHomework(@"Sarah", 3, 4);
+        
+        enumerateAllStudents();
+        removeStudent(@"Carol");
+        enumerateAllStudents();
+        removeAllStudents();
+        
+        //Student #6
+        addStudent(@"Claire", @"8 Willow Rd");
+        addTest(@"Claire", 100, @"midterm");
+        addTest(@"Claire", 100, @"final");
+        addHomework(@"Claire", 1, 10);
+        addHomework(@"Claire", 2, 10);
+        addHomework(@"Claire", 3, 10);
+        
+        //Student #7
+        addStudent(@"Emily", @"33 Northfield Rd");
+        addTest(@"Emily", 90, @"midterm");
+        addTest(@"Emily", 75, @"final");
+        addHomework(@"Emily", 1, 6);
+        addHomework(@"Emily", 2, 10);
+        addHomework(@"Emily", 3, 10);
+        
+        enumerateAllStudents();
     }
     return 0;
 }
